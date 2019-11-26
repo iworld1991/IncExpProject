16f6cca2d55d04681d9f5ce6d07571c2c1f9f7f9
Tao Wang <twang80@jhu.edu>
2019-11-26 10:59:48 -0500

    Revert "added first bash script to convert notebook to latex."
    This reverts commit c42ae242f3ffb78767b64a14c0061d41d134d863.

--- to_tex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd WorkingFolder/PythonCode/
-
-jupyter nbconvert PerceivedIncomeRisk.ipynb --to latex
-sed -r -i 's/documentclass\[15pt\]\{article\}/documentclass[8pt]{extarticle}/' PerceivedIncomeRisk.tex
-sed -r -i 's/geometry\{verbose,tmargin=1in,bmargin=1in,lmargin=1in,rmargin=1in}/geometry{verbose,tmargin=1in,bmargin=1in,lmargin=0.2in,rmargin=0.2in}/' PerceivedIncomeRisk.tex
-
-pdflatex PerceivedIncomeRisk.tex
